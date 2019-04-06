811892b4a6114b03fff2c1bd2156c5dbd8621832
ramchandravelip <velipramchandra@gmail.com>
2019-04-06 09:56:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 
-app.use(express.static(__dirname + '/dist/angularapp'));
+app.use(express.static(__dirname + '/angularapp'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/angularapp/index.html'));
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
  
 
